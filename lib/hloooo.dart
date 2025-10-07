f8ef66b3f9cbce98f418baeea4ab8ed120486474
Hasnain Akbar <malikhasnainakbar1@gmail.com>
2025-10-07 10:10:28 -0700

    That is my porovider code

---
+++ lib/hloooo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class asmat extends StatefulWidget {
+  const asmat({super.key});
+
+  @override
+  State<asmat> createState() => _asmatState();
+}
+
+class _asmatState extends State<asmat> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
